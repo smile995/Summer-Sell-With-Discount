f9d0b1127a1ba4367ad7e939b38f12167bc50769
smile995 <ah.smile995@gmai.com>
2023-08-21 21:18:05 +0600

    furniture section done

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -4,10 +4,9 @@ function handleClickOnDiv(target) {
     const selected = document.getElementById('selectedItemsContainer');
 
     const itemName = target.childNodes[3].childNodes[3].innerText;
-    console.log(itemName);
-    const h5 = document.createElement('p');
-    h5.innerText = itemName;
-    selected.append(h5)
+    const li = document.createElement('li');
+    li.innerText = itemName;
+    selected.append(li)
     const PriceString = target.childNodes[3].childNodes[5].childNodes[0].innerText;
     const priceNumber = parseFloat(PriceString)
     console.log(priceNumber);
@@ -31,7 +30,7 @@ function handleClickOnDiv(target) {
     couponText.value='';
     setTotal.innerText = '';
     document.getElementById('discount').innerText = '';
-    h5.innerText = '';
+    li.innerText = '';
   })
 
 
