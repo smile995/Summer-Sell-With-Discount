ae42c920d837693fd7c3e93009ef8dfb3503ca9e
smile995 <ah.smile995@gmai.com>
2023-08-21 17:29:40 +0600

    calculation update

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,14 +1,31 @@
 // console.log('connected');
-function handleClickOnDiv(target){
-   const selected= document.getElementById('selectedItemsContainer');
-    const itemName=target.childNodes[3].childNodes[3].innerText;
+let total = 0;
+function handleClickOnDiv(target) {
+    const selected = document.getElementById('selectedItemsContainer');
+
+    const itemName = target.childNodes[3].childNodes[3].innerText;
     console.log(itemName);
-    const h5=document.createElement('p');
-    h5.innerText=itemName;
+    const h5 = document.createElement('p');
+    h5.innerText = itemName;
     selected.append(h5)
-    const PriceString=target.childNodes[3].childNodes[5].childNodes[0].innerText;
-    const priceNumber=parseFloat(PriceString);
+    const PriceString = target.childNodes[3].childNodes[5].childNodes[0].innerText;
+    const priceNumber = parseFloat(PriceString)
     console.log(priceNumber);
+    total = total + priceNumber;
+    const setTotal = document.getElementById('totalPrice');
+    setTotal.innerText = total;
+
     
+    
+
+    if(total>=200){
+        const discount=(20*total)/100;
+        document.getElementById('discount').innerText=discount;
+        const AfterDiscount=total-discount;
+        document.getElementById('AfterDiscount').innerText=AfterDiscount;
+    }
+
+    
+
 
 }
