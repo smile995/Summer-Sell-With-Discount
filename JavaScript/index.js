8fa91c9f8b57b192f98bd4c2af1f1815b3e2fac5
smile995 <ah.smile995@gmai.com>
2023-08-21 16:13:45 +0600

    calculation part done

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,1 +1,14 @@
-console.log('connected');
+// console.log('connected');
+function handleClickOnDiv(target){
+   const selected= document.getElementById('selectedItemsContainer');
+    const itemName=target.childNodes[3].childNodes[3].innerText;
+    console.log(itemName);
+    const h5=document.createElement('p');
+    h5.innerText=itemName;
+    selected.append(h5)
+    const PriceString=target.childNodes[3].childNodes[5].childNodes[0].innerText;
+    const priceNumber=parseFloat(PriceString);
+    console.log(priceNumber);
+    
+
+}
